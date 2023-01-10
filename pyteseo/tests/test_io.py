057c6cc3b1580bcb362505758b776dfe35c434fa
aragong <german.aragon@unican.es>
2023-01-10 13:18:20 +0100

    update

--- pyteseo/tests/test_io.py
+++ pyteseo/tests/test_io.py
@@ -17,10 +17,10 @@ from shutil import rmtree
 import pytest
 from pyteseo.__init__ import __version__ as v
 
-data_path = Path(__file__).parent / "data"
-tmp_path = Path(f"./tmp_pyteseo_{v}_tests")
 
 # TODO - Put a @fixture to setup the base path
+data_path = Path(__file__).parent / "data"
+tmp_path = Path(f"./tmp_pyteseo_{v}_tests")
 
 
 @pytest.mark.parametrize(
