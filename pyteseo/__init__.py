02365e9364443804ba711a4dc7219b0d9af50ee5
aragong <german.aragon@unican.es>
2023-01-11 18:06:42 +0100

    update for v0.0.4

--- pyteseo/__init__.py
+++ pyteseo/__init__.py
@@ -1,4 +1,4 @@
 """Python package developed to simplify and facilitate the setup and processing of TESEO simulations (TESEO is a lagrangian numerical model developed by IHCantabria)
 """
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
