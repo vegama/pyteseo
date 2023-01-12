7005ee54455beabbca0adfb071fa56bcb8946851
aragong <german.aragon@unican.es>
2023-01-12 16:51:11 +0100

    update cli tests structure

--- pyteseo/cli.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-import pytest
-
-def run_tests():
-    """run all available tests"""
-    tests_path = Path(__file__).parent
-    print(f"Running tests from path: {tests_path}")
-    pytest.main(["-v", "--durations=0", "--durations-min=0.05", tests_path])
--- pyteseo/tests/__init__.py
+++ pyteseo/tests/__init__.py
@@ -1,3 +1,12 @@
 """Testing subpackage for pyteseo"""
 
 __all__ = []
+
+from pathlib import Path
+import pytest
+
+def run_tests():
+    """run all available tests"""
+    tests_path = Path(__file__).parent
+    print(f"Running tests from path: {tests_path}")
+    pytest.main(["-v", "-p" "no:warnings", tests_path])
